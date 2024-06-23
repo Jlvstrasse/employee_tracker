d9880496e07a8af393fa9cd4ce63bed308f94b99
Jlvstrasse <jlvstrasse@gmail.com>
2024-06-23 19:44:39 -0400

    sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,3 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
-
-\c employees_db;
- 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
@@ -10,35 +5,16 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    department_id INTEGER NOT NULL REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    FOREIGN KEY (role_id) 
-    REFERENCES role(id),
-    manager_id INTEGER,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    role_id INTEGER NOT NULL REFERENCES role(id),
+    manager_id INTEGER REFERENCES employee(id)
 );
 
-ALTER TABLE role
-DROP CONSTRAINT role_department_id_fkey,
-ADD CONSTRAINT role_department_id_fkey
-FOREIGN KEY (department_id)
-REFERENCES department (id)
-ON DELETE CASCADE;
-
-ALTER TABLE employee
-DROP CONSTRAINT employee_role_id_fkey,
-ADD CONSTRAINT employee_role_id_fkey
-FOREIGN KEY (role_id)
-REFERENCES role (id)
-ON DELETE CASCADE;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,17 @@
-INSERT INTO department (name) 
-  VALUES 
-  ('Sales'), ('Engineering'), ('Finance');
+INSERT INTO department (name) VALUES 
+('Sales'), 
+('Engineering'), 
+('Finance');
 
 INSERT INTO role (title, salary, department_id) VALUES
-  ('Sales Manager', 75000, 1),
-  ('Salesperson', 50000, 1),
-  ('Software Engineer', 80000, 2),
-  ('Accountant', 60000, 3);
+('Sales Manager', 75000, 1),
+('Salesperson', 50000, 1),
+('Software Engineer', 80000, 2),
+('Accountant', 60000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-  ('John', 'Doe', 1, NULL),
-  ('Jane', 'Smith', 2, 1),
-  ('Mike', 'Johnson', 3, NULL),
-  ('Emily', 'Davis', 4, NULL);
+('John', 'Doe', 1, NULL),
+('Jane', 'Smith', 2, 1),
+('Mike', 'Johnson', 3, NULL),
+('Emily', 'Davis', 4, NULL);
+
